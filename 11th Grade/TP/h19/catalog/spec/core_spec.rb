131aba26355e97b18e205b7998017e06af9856d1
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2013-04-06 19:17:00 +0300

    Almost ready. This commit message sucks.

--- 11th Grade/TP/h19/catalog/spec/core_spec.rb
+++ 11th Grade/TP/h19/catalog/spec/core_spec.rb
@@ -5,7 +5,6 @@ describe Collection do
 		@songs = File.open("#{Dir.pwd}/catalog/songs.txt", "r").read
 		@artists = File.open("#{Dir.pwd}/catalog/artists.txt", "r").read
 		@c = Collection.new(@songs, @artists)
-		@c.parse
 	end
 
 	it "should reads songs from file" do
@@ -21,7 +20,7 @@ describe Collection do
 		@c.songs.first.artist.should eq('John Coltrane')
 		@c.songs.first.genre.should eq('Jazz')
 		@c.songs.first.subgenre.should eq('Bebop')
-		@c.songs.first.tags.should eq(['popular', 'cover'])
+		@c.songs.first.tags.should include('popular', 'cover')
 
 		@c.songs.last.subgenre.should eq(nil)
 	end
@@ -37,5 +36,26 @@ describe Collection do
 	it "should can find song by its artists name" do
 		@c.find(artist: 'John Coltrane').first.name.should eq('My Favourite Things')
 		@c.find(artist: 'Bach').last.name.should eq('Goldberg Variations')
+	end
+
+	it "should add genre and subgenre to tags (smallcased)" do
+		@c.find(tags: %w[jazz bebop]).first.name.should eq('My Favourite Things')
+	end
+
+	it "should add the info from the artists dictionary to the catalog" do
+		@c.find(tags: %w[saxophone]).first.name.should eq('My Favourite Things')
+		@c.find(tags: 'saxophone').first.name.should eq('My Favourite Things')
+	end
+
+	it "should have option to make negative calls to 'find' with !" do
+		@c.find(tags: %w[jazz cover!]).first.name.should eq('Alabama')
+	end
+
+	it "should return all songs if 'find' receive no arguments" do
+		@c.find().count.should eq(20)
+	end
+
+	it "should return empty list if 'find' have no matches" do
+		@c.find(artist: 'Lil Wayne').should eq([]) # Lil is not an artist = /
 	end
 end
--- 11th Grade/TP/h19/catalog/src/core.rb
+++ 11th Grade/TP/h19/catalog/src/core.rb
@@ -1,13 +1,41 @@
+Array.class_eval do
+	def stay what
+		return reject {|x| x.end_with? '!'} if what == :+
+		select {|x| x.end_with? '!'} if what == :-
+	end
+end
+
 class Song
 	attr_accessor :name, :artist, :genre, :subgenre, :tags
 
 	def initialize name, artist, genre, subgenre, tags
-		@name, @artist, @genre, @subgenre, @tags = name, artist, genre, subgenre, tags
+		@name, @artist, @genre, @subgenre = name, artist, genre, subgenre
+		@tags = Array(tags)
+	end
+
+	def match_tags? values
+		return false unless (@tags & values).count == values.stay(:+).count
+		if values.stay(:-).count > 0
+			return false if (@tags - values).include? *values.stay(:-).map {|x| x.chop}
+		end
+		true
 	end
 
 	def match? criteria
-		criteria.keys.map {|c| return false unless eval("@#{c.to_s}") == criteria[c]}
+		criteria.keys.each do |key|
+			if key == :tags
+				return match_tags? Array(criteria[key])
+			else
+				return false unless eval("@#{key.to_s}") == criteria[key]
+			end
+		end
 		true
+	end
+
+	def load_tags! tags
+		@tags |= tags
+		@tags << @genre.downcase
+		@tags << @subgenre.downcase if @subgenre
 	end
 end
 
@@ -18,18 +46,26 @@ class Collection
 		@songs_as_strings = songs.lines.to_a.map {|l| l.chomp}
 		@artist_tags = eval(artists)
 		@songs = Array.new
+		parse!
+		load_songs_tags!
 	end
 
-	def find criteria
+	def find criteria = {}
 		@songs.select {|s| s.match?(criteria)}
 	end
 
-	def parse
+	def parse!
 		songs_as_strings.map do |line|
 			n, a, gs, t = line.split('.').map {|s| s.strip}
 			g, s = gs.split(',').map {|x| x.strip}
 			t = t.split(',').map {|x| x.strip} unless t.nil?
 			@songs << Song.new(n, a, g, s, t)
+		end
+	end
+
+	def load_songs_tags!
+		@artist_tags.map do |artist, tags|
+			find(artist: artist).map {|s| s.load_tags! tags}
 		end
 	end
 end
